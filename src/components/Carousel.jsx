ed07ae720cf9732f367f1a2f5d8f7b985ec938c9
Craig Pearson <thepearson@gmail.com>
2022-05-23 07:36:08 +1200

    Repo setup, build scripts etc

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const items = [
+  {
+    title: ''
+  }
+];
+
+export default function Carousel() {
+  return (
+    <div>
+      <h1>A Carousel</h1>
+    </div>
+  )
+}
